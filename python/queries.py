210700e2424057a584ae47135857addf59ffb060
JohnRFoster <fosterj@bu.edu>
2026-03-17 15:09:35 -0400

    new sql pull

--- python/queries.py
+++ python/queries.py
@@ -7,57 +7,96 @@ import oracledb
 def single(cursor):
     cursor.execute(
         """
-    select m2_work_task.id as wt_id,
-    M2_STATE.ST_ABBR,
+   SELECT
+    -- Core IDs
+    M2_WORK_TASK.ID AS WT_ID,
+    M2_AGREEMENT_PROPERTY.AGRP_PRP_ID,
+    M2_WORK_TASK.WT_AGRPROP_ID,
+
+    -- Property information (LEFT JOINs per Will semantics)
+    M2_PROPERTY.PRP_NAME,
+    M2_COUNTY.CNTY_NAME,
+    M2_COUNTY.CNTY_GSA_CNTY_CD,
     M2_STATE.ST_NAME,
-    m2_county.cnty_name,
-    m2_property.prp_name,
-    m2_project.proj_name,
-    m2_work_task.wt_work_date,
-    m2_employee.emp_last_name,
-    m2_employee.emp_first_name,
-    m2_component.cmp_name,
-    m2_work_task_comp_uom.wtcm_qty as CMP_QTY,
-    M2_USE_TYPE.USET_NAME as USE_TYPE,
-    m2_fate.fate_fate,
-    m2_work_result.wkr_qty as TAKE,
+    M2_STATE.ST_GSA_STATE_CD,
+    M2_PROPERTY_SIZE.PRPS_QTY,
+    M2_PROPERTY_SIZE.PRPS_PROP_TYPE,
+    M2_PROPERTY_UOL.PRPU_N_LAT,
+    M2_PROPERTY_UOL.PRPU_E_LONG,
+    M2_UNIT_OF_MEASURE.UOM_NAME AS PROPERTY_SIZE_UOM,
+
+    -- Allowed species (kept for ALWS_AGRPROP_ID)
+    M2_ALLOWED_SPECIES.ALWS_AGRPROP_ID,
+
+    -- Work task info
+    M2_WORK_TASK.WT_WORK_DATE,
+
+    -- Component & usage info
+    M2_COMPONENT.CMP_NAME,
+    M2_COMPONENT.CMP_TYPE,
+    M2_USE_TYPE.USET_NAME,
+    M2_WORK_TASK_COMP_UOM.WTCM_QTY,
+
+    -- Damage agent (driven by work_result to match Will)
+    M2_DAMAGE_AGENT.ID AS DAMAGE_AGENT_ID,
+    M2_DAMAGE_AGENT.DA_NAME,
+
+    -- Fate & work result (inner joins + killed filter)
+    M2_FATE.FATE_WKR_ID,
+    M2_FATE.FATE_FATE,
+    M2_WORK_RESULT.WKR_QTY,
     M2_WORK_RESULT.WKR_INTENTIONAL,
-    M2_WORK_RESULT.WKR_TARGET,
-    M2_WORK_TASK.WT_ENTRY_DATE,
-    m2_damage_agent.da_name
-from m2_work_task
+    M2_WORK_RESULT.WKR_TARGET
+
+FROM M2_WORK_TASK
+
+    -- Components (required)
     INNER JOIN M2_WORK_TASK_COMPONENT
         ON M2_WORK_TASK_COMPONENT.WTC_WT_ID = M2_WORK_TASK.ID
-    inner join m2_component
-        on m2_work_task_component.wtc_cmp_id=m2_component.id
-    inner join m2_fate
-        on m2_fate.fate_wtc_id = m2_work_task_component.id
-    inner join m2_work_result
-        on m2_work_result.id = m2_fate.fate_wkr_id
-    inner join m2_damage_agent
-        on m2_damage_agent.id = m2_work_result.wkr_da_id
-    left join m2_work_project
-        on m2_work_task.id=m2_work_project.wkpr_wt_id
-    left join m2_project
-        on m2_work_project.wkpr_proj_id = m2_project.id
-    left join m2_work_task_comp_uom
-        on m2_work_task_component.id = m2_work_task_comp_uom.wtcm_wtc_id
+    INNER JOIN M2_COMPONENT
+        ON M2_COMPONENT.ID = M2_WORK_TASK_COMPONENT.WTC_CMP_ID
+
+    -- Use Type (optional in Will, so LEFT)
+    LEFT JOIN M2_USE_TYPE
+        ON M2_USE_TYPE.ID = M2_WORK_TASK_COMPONENT.WTC_USET_ID
+
+    -- Component UOM (optional in Will, so LEFT)
+    LEFT JOIN M2_WORK_TASK_COMP_UOM
+        ON M2_WORK_TASK_COMPONENT.ID = M2_WORK_TASK_COMP_UOM.WTCM_WTC_ID
+
+    -- Agreement & property (optional per Will, so LEFT)
     LEFT JOIN M2_AGREEMENT_PROPERTY
         ON M2_AGREEMENT_PROPERTY.ID = M2_WORK_TASK.WT_AGRPROP_ID
     LEFT JOIN M2_PROPERTY
         ON M2_PROPERTY.ID = M2_AGREEMENT_PROPERTY.AGRP_PRP_ID
-    LEFT JOIN M2_PROPERTY_UOL
-        ON M2_PROPERTY_UOL.PRPU_PRP_ID = M2_PROPERTY.ID
-    left join m2_county
-        on m2_county.id = m2_property.prp_cnty_id
+    LEFT JOIN M2_COUNTY
+        ON M2_COUNTY.ID = M2_PROPERTY.PRP_CNTY_ID
     LEFT JOIN M2_STATE
         ON M2_STATE.ID = M2_PROPERTY.PRP_ST_ID
-    LEFT JOIN M2_USE_TYPE
-        ON M2_USE_TYPE.ID = M2_WORK_TASK_COMPONENT.WTC_USET_ID
-    left join m2_employee
-        on m2_employee.id = m2_work_task.wt_emp_id
-WHERE M2_DAMAGE_AGENT.ID = 8 AND
-    M2_FATE.FATE_FATE = 'KILLED'      
+    LEFT JOIN M2_PROPERTY_SIZE
+        ON M2_PROPERTY.ID = M2_PROPERTY_SIZE.PRPS_PRP_ID
+    LEFT JOIN M2_UNIT_OF_MEASURE
+        ON M2_UNIT_OF_MEASURE.ID = M2_PROPERTY_SIZE.PRPS_UOM_ID
+    LEFT JOIN M2_PROPERTY_UOL
+        ON M2_PROPERTY.ID = M2_PROPERTY_UOL.PRPU_PRP_ID
+
+    -- Allowed species (optional; included to surface ALWS_AGRPROP_ID)
+    LEFT JOIN M2_ALLOWED_SPECIES
+        ON M2_ALLOWED_SPECIES.ALWS_AGRPROP_ID = M2_AGREEMENT_PROPERTY.ID
+
+    -- Fate & work result (match Will: inner joins, tied to component)
+    INNER JOIN M2_FATE
+        ON M2_FATE.FATE_WTC_ID = M2_WORK_TASK_COMPONENT.ID
+    INNER JOIN M2_WORK_RESULT
+        ON M2_WORK_RESULT.ID = M2_FATE.FATE_WKR_ID
+
+    -- Damage agent from work_result (match Will)
+    INNER JOIN M2_DAMAGE_AGENT
+        ON M2_DAMAGE_AGENT.ID = M2_WORK_RESULT.WKR_DA_ID
+
+WHERE
+    M2_FATE.FATE_FATE = 'KILLED' AND
+    M2_WORK_TASK.WT_WORK_DATE >= to_date(' 2001-01-01 ','yyyy-mm-dd')      
           """
     )
 
